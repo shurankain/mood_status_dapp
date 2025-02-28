9376a3a3b87e7fa8138e70e9f4f0bf673fa66318
Oleksandr_Husiev <ohusiev@icloud.com>
2025-02-28 22:33:57 +0100

    added mood processing logic

--- programs/mood_status_dapp/src/lib.rs
+++ programs/mood_status_dapp/src/lib.rs
@@ -1,16 +1,40 @@
 use anchor_lang::prelude::*;
 
-declare_id!("6iuP8j1RfwRyHCDUdtrSv3GAtZa9ZKkePMVVbNRXMoH8");
+declare_id!("5eWpbFtEPydGTGaMEhLBoogRBU7sd7cU4AVfwCC8U1sA");
 
 #[program]
 pub mod mood_status_dapp {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
+      let base_account = &mut ctx.accounts.base_account;
+      base_account.mood = String::from("Happy");
+      Ok(())
+  }
+
+  pub fn update(ctx: Context<Update>, mood: String) -> Result<()> {
+      let base_account = &mut ctx.accounts.base_account;
+      base_account.mood = mood;
+      Ok(())
+  }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 50)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub mood: String,
+}
